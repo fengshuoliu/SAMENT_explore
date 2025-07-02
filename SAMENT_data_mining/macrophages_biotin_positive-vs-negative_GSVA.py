d4dc4009d9adf6494584082f1eb196fd31a1f67d
fengshuoliu <116980671+fengshuoliu@users.noreply.github.com>
2025-07-02 15:01:02 -0500

    Update macrophages_biotin_positive-vs-negative_GSVA.py

--- SAMENT_data_mining/macrophages_biotin_positive-vs-negative_GSVA.py
+++ SAMENT_data_mining/macrophages_biotin_positive-vs-negative_GSVA.py
@@ -80,7 +80,19 @@ def wrap_text(text, width=30):
 def update_plot(keywords=[], exclude_keywords=[], logic='AND',
                 neg_cutoff=-0.2, pos_cutoff=0.2, significance_cutoff=1.0,
                 width='100%', height=800, interactive=True):
-    df['category'] = df.apply(get_category, axis=1, keywords=keywords, exclude_keywords=exclude_keywords, logic=logic)
+    df['category'] = df.apply(
+    lambda row: get_category(
+          row,
+          neg_cutoff=neg_cutoff,
+          pos_cutoff=pos_cutoff,
+          significance_cutoff=significance_cutoff,
+          keywords=keywords,
+          exclude_keywords=exclude_keywords,
+          logic=logic
+        ),
+        axis=1
+    )
+
     palette = {'keyword_match': '#32CD32', 'upregulated': '#FF6347', 'downregulated': '#1E90FF', 'non-significant': '#A9A9A9'}
     fig = go.Figure()
     
