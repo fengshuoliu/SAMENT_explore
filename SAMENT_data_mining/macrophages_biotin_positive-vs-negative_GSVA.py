9e08dc1665e3a126045944b150fe46fbac4e79ff
fengshuoliu <116980671+fengshuoliu@users.noreply.github.com>
2025-07-02 14:54:13 -0500

    Update macrophages_biotin_positive-vs-negative_GSVA.py

--- SAMENT_data_mining/macrophages_biotin_positive-vs-negative_GSVA.py
+++ SAMENT_data_mining/macrophages_biotin_positive-vs-negative_GSVA.py
@@ -92,7 +92,8 @@ def update_plot(keywords=[], exclude_keywords=[], logic='AND', neg_cutoff=-0.2, 
     upregulated_df = df[df['category'] == 'upregulated']
     fig.add_trace(go.Scatter(x=upregulated_df['GSVA_score'], y=upregulated_df['-log10(adj.P.Val)'], mode='markers',
                              marker=dict(size=8, color=palette['upregulated'], opacity=0.8, line=dict(width=0.5, color='black')),
-                             text=[name for name in upregulated_df.index]
+                             text=[f'<span style="color:{palette["upregulated"]};">{name}</span>' for name in upregulated_df.index]
+
 
     # Plot down-regulated pathways
     downregulated_df = df[df['category'] == 'downregulated']
